e709733a1f4d8b9314c7062ef972147359624d50
Dorstener <nicolas.arbeletche@gmail.com>
2024-04-05 10:25:24 +0200

    step 15 completed

---
+++ index.js
@@ -0,0 +1,3 @@
+const calorieCounter = document.getElementById("calorie-counter");
+let budgetNumberInput = document.getElementById("budget");
+let entryDropdown = document.getElementById("entry-dropdown");
