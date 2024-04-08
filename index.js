842fe17c362aa56a7690849426804576d582d1ee
Dorstener <nicolas.arbeletche@gmail.com>
2024-04-08 18:24:12 +0200

    step 23

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const calorieCounter = document.getElementById("calorie-counter");
 let budgetNumberInput = document.getElementById("budget");
 let entryDropdown = document.getElementById("entry-dropdown");
+let addEntryButton = document.getElementById("add-entry");
+let clearButton = document.getElementById("clear");
+let output = document.getElementById("output");
+let isError = false;
+
+
+// FUNCTIONS //
+
+let cleanInputString = function cleanInputString(str) {
+    const regex = /hello/;
+}
