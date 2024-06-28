9d5e2c2fe7e3bcddb8b4109911e2c2622adbc281
Dorstener <nicolas.arbeletche@gmail.com>
2024-06-28 13:45:04 +0200

    step 63 done

--- index.js
+++ index.js
@@ -12,20 +12,40 @@ let cleanInputString = function cleanInputString(str) {
 
 // FUNCTIONS //
 
-let isInvalidInput = function isInvalidInput (str) {
+let isInvalidInput = function isInvalidInput(str) {
     const regex = /\d+e\d+/i;
     return str.match(regex);
 }
 
-function addEntry () {
+function addEntry() {
     let targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-    let entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+    let entryNumber = ((targetInputContainer.querySelectorAll('input[type="text"]').length) + 1);
     let HTMLString = `
     <label for="${entryDropdown.value}-${entryNumber}-name"> Entry ${entryNumber} Name </label>
     <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name">
     <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
     <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories">
     `;
-    targetInputContainer.innerHTML += HTMLString;
+    targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
+
 };
+
+
+function getCaloriesFromInputs(list) {
+
+    let calories = 0;
+
+    for (const item of list) {
+        const currVal = cleanInputString(item.value);
+        let invalidInputMatch = isInvalidInput(currVal);
+        if (invalidInputMatch) {
+            alert(`Invalid Input:${invalidInputMatch[0]}`);
+        }
+    }
+}
+
+// EVENTS //
+
+addEntryDropdown.addEventListener('click', addEntry);
+
 
